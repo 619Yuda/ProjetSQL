1b745b4c9ea866765975ca845f0cd55fba73e645
Adrien Leger <adrien.leger@gmail.com>
2014-01-29 12:18:47 +0100

    refactoring

--- data/CsvToSqlOracle.sh
+++ data/CsvToSqlOracle.sh
@@ -1,20 +1,21 @@
 echo -e "-- File created by CsvToSql.sh $(date)\n-- Conversion from Oracle to psql\n" > Data.sql
 
 for i in $*; do
+    
     bn=$(basename $i .csv)
-    echo -e "Processing $bn"
 
-    head -n 1 "$i"\
-    | tr '\t' ','\
-    | sed 's/\(^.*$\)/\nINSERT INTO '$bn' (\1) VALUES/g' >> Data.sql
+    var=$(head -n 1 "$i" | tr '\t' ',')
+   
+    echo -e "\nINSERT ALL" >> Data.sql
 
     tail "$i" -n +2\
     | tr '\t' ','\
     | sed 's/\([^,]*\)/#\1#/g'\
-    | sed 's/\(^.*$\)/(\1),/g'\
-    | sed '$ s/,$/;/g' \
+    | sed 's/\(^.*$\)/  INTO '$bn' ('$var') VALUES (\1)/g'\
+    | sed '$ s/)$/)\nSELECT \* FROM dual;/g' \
     | sed 's/#\([0-9]\{1,4\}\)#/\1/g' \
     | sed 's/#NULL#/NULL/g' \
     | sed 's/#\(Sysdate-[0-9]\{1,4\}\)#/\1/g' \
     | tr '#' ''\''' >> Data.sql
+
 done;
