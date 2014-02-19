f846045a2d55f5ef1aec047f5055acb2f18da36c
Charlotte <charlotte.rieux44@yahoo.fr>
2014-02-19 19:03:45 +0100

    Correction of the 8th .sql file

--- Oracle/8_Triggers.sql
+++ Oracle/8_Triggers.sql
@@ -2,7 +2,7 @@
 suppression du dernier exemplaire d’un ouvrage, les informations relatives à l’ouvrage sont
 également supprimées. */
 
-CREATE OR REPLACE TRIGGER suppression_si_aucun_exemplaire
+CREATE OR REPLACE TRIGGER trg_del_exemplaire
 	AFTER DELETE ON Exemplaire
 	FOR EACH ROW
 DECLARE
@@ -10,48 +10,48 @@ DECLARE
 BEGIN
 	SELECT count(*) INTO v_nombre_exemplaire FROM Exemplaire WHERE isbn = :old.isbn;
 	IF (v_nombre_exemplaire = 1) THEN
-		DELETE FROM Ouvrages WHERE isbn = :old.isbn;
-	ENDIF;
+		DELETE FROM Ouvrage WHERE isbn = :old.isbn;
+	END IF;
 END;
 
 /* 2 Définir un déclencheur de base de données permettant de garantir que les emprunts sont
 réalisés uniquement par des membres à jour de leur cotisation.*/
 
-CREATE OR REPLACE TRIGGER emprunt_autorisé
+CREATE OR REPLACE TRIGGER trg_autorisation
 	BEFORE INSERT ON Emprunt
 	FOR EACH ROW
 DECLARE
 	emprunt_bool BOOLEAN;
 BEGIN
-	emprunt_bool := AdhesionAJour(:old.numero_membre)
-	IF(!emprunt_bool)
+	emprunt_bool := AdhesionAJour(:old.numero_membre);
+	IF NOT emprunt_bool THEN
 		RAISE_APPLICATION_ERROR(-20200,'Adhesion non valide');
-	ENDIF;
+	END IF;
 END;
 
 /* 3 Définir un déclencheur qui interdit le changement de membre pour une fiche de location
 déjà enregistrée.*/
 
-CREATE OR REPLACE TRIGGER changements_membre_sur_emprunt_interdits
+CREATE OR REPLACE TRIGGER trg_interdit_chang_emprunt
 	BEFORE UPDATE OF numero_membre ON Emprunt
 	FOR EACH ROW
 BEGIN
-	IF(:new.numero_membre != :old.numero_membre)
+	IF(:new.numero_membre != :old.numero_membre) THEN
 		RAISE_APPLICATION_ERROR(-20200,'Opération interdite');
-	ENDIF;
+	END IF;
 END;
 
 /* 4 Définir un déclencheur qui interdit de modifier la référence d’un ouvrage emprunté, il faut
 le rendre puis effectuer une nouvelle location */ 
 -- C'est quoi la référence ?!
 
-CREATE OR REPLACE TRIGGER changements_details_emprunt_interdits
+CREATE OR REPLACE TRIGGER trg_intedit_modif_ref
 	BEFORE UPDATE OF isbn ON Details
 	FOR EACH ROW
 BEGIN
-	IF(:new.isbn != :old.isbn)
+	IF(:new.isbn != :old.isbn) THEN
 		RAISE_APPLICATION_ERROR(-20200,'Opération interdite');
-	ENDIF;
+	END IF;
 END;
 
 /* 5 Définir un déclencheur qui met automatiquement à jour l’état d’un exemplaire en fonction
@@ -59,38 +59,42 @@ de la valeur enregistrée dans NombreEmprunts. Par exemple, lors de la mise à j
 valeurs représentant le nombre d’emprunts pour un exemplaire, l’état est mis à jour de façon
 automatique. */
 
-CREATE OR REPLACE TRIGGER maj_etat_exemplaire_automatique
-	AFTER INSERT OR UPDATE OF nombreEmprunts ON Exemplaire
+CREATE OR REPLACE TRIGGER trg_maj_etat_ex
+	BEFORE INSERT OR UPDATE OF nombre_emprunts ON Exemplaire
 	FOR EACH ROW
 BEGIN
-	IF(:new.nombreEmprunts <= 10) THEN
-		:new.etat := 'NE';
-	ENDIF;
-	IF(:new.nombreEmprunts BETWEEN 11 AND 25) THEN
-		:new.etat := 'BO';
-	ENDIF;
-	IF(:new.nombreEmprunts BETWEEN 26 AND 40) THEN
-		:new.etat := 'MO';
-	ENDIF;
-	IF(:new.nombreEmprunts BETWEEN 41 AND 60) THEN
-		:new.etat := 'DO';
-	ENDIF;
-	IF(:new.nombreEmprunts >= 61) THEN
-		:new.etat := 'MA';
-	ENDIF;
+	IF(:new.nombre_emprunts <= 10) THEN
+		:new.etat := 'Neuf';
+	END IF;
+	IF(:new.nombre_emprunts BETWEEN 11 AND 25) THEN
+		:new.etat := 'Bon';
+	END IF;
+	IF(:new.nombre_emprunts BETWEEN 26 AND 40) THEN
+		:new.etat := 'Moyen';
+	END IF;
+	IF(:new.nombre_emprunts BETWEEN 41 AND 60) THEN
+		:new.etat := 'Douteux';
+	END IF;
+	IF(:new.nombre_emprunts >= 61) THEN
+		:new.etat := 'Mauvais';
+	END IF;
 END;
 
 /* 6 Lors de la suppression d’un détail, assurer que l’emprunt a bien été pris en compte au
 niveau de l’exemplaire. */
 
-CREATE OR REPLACE TRIGGER maj_nombre_emprunt_exemplaire
-	BEFORE DELETE ON Details
+CREATE OR REPLACE TRIGGER trg_suppr_detail
+	AFTER DELETE ON Details
 	FOR EACH ROW
 DECLARE
-	v_isbn := old.isbn;
-	v_exemplaire := old.exemplaire;
+	v_isbn Details.isbn%type;
+	v_exemplaire Details.exemplaire%type;
 BEGIN
-	UPDATE Exemplaire SET nombreEmprunt = ((SELECT nombreEmprunt FROM Exemplaire WHERE Exemplaire.isbn = v_isbn and Exemplaire.numero_exemplaire = v_exemplaire)+1) 
+	SELECT isbn INTO v_isbn FROM Details 
+	WHERE isbn = :old.isbn;
+	SELECT exemplaire INTO v_exemplaire FROM Details
+	WHERE exemplaire = :old.exemplaire;
+	UPDATE Exemplaire SET nombre_emprunts = ((SELECT nombre_emprunts FROM Exemplaire WHERE Exemplaire.isbn = v_isbn and Exemplaire.numero_exemplaire = v_exemplaire) + 1) 
 	WHERE Exemplaire.isbn = v_isbn and Exemplaire.numero_exemplaire = v_exemplaire;
 END;
 
@@ -106,9 +110,9 @@ DROP SEQUENCE seq_numero_employe;
 CREATE SEQUENCE seq_numero_employe START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE Employe (
-	nom VARCHAR2(10) NOT NULL;
-	prenom VARCHAR2(10) NOT NULL;
-	numero_employe INTEGER NOT NULL;
+	nom VARCHAR2(10) NOT NULL,
+	prenom VARCHAR2(10) NOT NULL,
+	numero_employe INTEGER NOT NULL,
 -----------------------------------------------
 	CONSTRAINT pk_employé PRIMARY KEY (numero_employe);
 );
@@ -121,17 +125,19 @@ ALTER TABLE Details ADD numero_employe INTEGER NOT NULL;
 ALTER TABLE Details_Emprunt
     ADD CONSTRAINT fk_numero_employe FOREIGN KEY (numero_employe) REFERENCES Employe(numero_employe);
 
--------2eme version selon le livre
+-------2eme version 
 
 ALTER TABLE Emprunt ADD(
-	employe VARCHAR2(10);
-	date_modif date);
+	employe VARCHAR2(10),
+	date_modif date
+);
 	
 ALTER TABLE Details ADD(
-	employe VARCHAR2(10);
-	date_modif date);
+	employe VARCHAR2(10),
+	date_modif date
+);
 	
-CREATE OR REPLACE TRIGGER enr_operation_emprunt
+CREATE OR REPLACE TRIGGER trg_operation_emprunt
 	BEFORE INSERT ON Emprunt
 	FOR EACH ROW
 BEGIN
@@ -139,7 +145,7 @@ BEGIN
 	:new.date_modif := sysdate();
 END;
 
-CREATE OR REPLACE TRIGGER enr_operation_details
+CREATE OR REPLACE TRIGGER trg_operation_details
 	BEFORE INSERT ON Details
 	FOR EACH ROW
 BEGIN
@@ -155,9 +161,9 @@ fonction est toujours un nombre entier.*/
 CREATE OR REPLACE FUNCTION AnalyseActivite (v_nom_utilisateur VARCHAR2(10), v_date DATE) RETURN INTEGER AS
 	v_nombre_operations INTEGER;
 BEGIN
-	v_nombre_operation := (SELECT count(*) FROM flashback_transaction_query
-							WHERE table_name = 'Details' AND table_name = 'Emprunt' AND xid = v_nom_utilisateur AND start_timestamp >= v_date;
-							GROUP BY xid;
+	SELECT count(*) INTO v_nombre_operations FROM flashback_transaction_query
+	WHERE table_name = 'Details' AND table_name = 'Emprunt' AND xid = v_nom_utilisateur AND start_timestamp >= v_date
+	GROUP BY xid;
 	Return v_nombre_operation;
 END;
 
@@ -175,5 +181,5 @@ BEGIN
 	v_nombre_livre_rendu := (SELECT count(*) FROM Details WHERE Details.numero_emprunt = :old.numero_emprunt AND etat = 'RE' GROUP BY Details.numero_emprunt)
 	IF(v_nombre_livre == v_nombre_livre_rendu) THEN
 		RAISE_APPLICATION_ERROR(-20200,'Opération interdite');
-	ENDIF;
+	END IF;
 END;
